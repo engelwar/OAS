d7bb3ec2935418327fc51e87f7138898631608f4
sullca22 <juliocesarmancillaruiz@gmail.com>
2022-04-08 16:18:19 -0400

    Actualizado

--- resources/views/reports/vista/cuentasporcobrar.blade.php
+++ resources/views/reports/vista/cuentasporcobrar.blade.php
@@ -21,11 +21,11 @@ table.dataTable {
             <table id="example" class="cell-border compact hover" style="width:100%">
             <tfoot class="table tabll-sm border">
                 <tr class="text-right">
-                <th colspan = 3></th>
+                <th colspan = 5></th>
                 <th>TOTAL</th>
-                <td class="sumTCXC"></td>
-                <td class="sumTCuenta"></td>
-                <td class="sumTSaldo"></td>
+                <td class="sumTCXC bg-success"></td>
+                <td class="sumTCuenta bg-warning"></td>
+                <td class="sumTSaldo bg-info"></td>
                 <th colspan = 7></th>
                 <!--/tr>
                 <tr class="text-right">
@@ -35,27 +35,27 @@ table.dataTable {
                 </tr-->
                 
                 <tr class="text-right">
-                <td colspan = 3></td>
+                <td colspan = 5></td>
                 <td>VIGENTE</td>
-                <td class="sumVigenteCXC"></td>            
-                <td class="sumVigenteCuenta"></td>
-                <td class="sumVigenteSaldo"></td>
-                <td colspan = 8></td>
+                <td class="sumVigenteCXC bg-success"></td>            
+                <td class="sumVigenteCuenta bg-warning"></td>
+                <td class="sumVigenteSaldo bg-info"></td>
+                <td colspan = 7></td>
                 </tr>
                 <tr class="text-right">
-                <td colspan = 3></td>
+                <td colspan = 5></td>
                 <td>VENCIDO</td>
-                <td class="sumVencidoCXC"></td>            
-                <td class="sumVencidoCuenta"></td>
-                <td class="sumVencidoSaldo"></td>
+                <td class="sumVencidoCXC bg-success"></td>            
+                <td class="sumVencidoCuenta bg-warning"></td>
+                <td class="sumVencidoSaldo bg-info"></td>
                 <td colspan = 8></td>
                 </tr>
                 <tr class="text-right">
-                <td colspan = 3></td>
+                <td colspan = 5></td>
                 <td>MORA</td>
-                <td class="sumMoraCXC"></td>            
-                <td class="sumMoraCuenta"></td>
-                <td class="sumMoraSaldo"></td>
+                <td class="sumMoraCXC bg-success"></td>            
+                <td class="sumMoraCuenta bg-warning"></td>
+                <td class="sumMoraSaldo bg-info"></td>
                 <td colspan = 8></td>
             </tr>
             </tfoot>
@@ -97,8 +97,8 @@ $(document).ready(function()
             { data: 'FechaVenc', title: 'FechaVenc'},
             { data: 'ImporteCXC', title: 'ImporteCXC'},
             { data: 'ACuenta', title: 'ACuenta'},
-            { data: 'FechaCobro', title: 'FechaCobro'},
             { data: 'Saldo', title: 'Saldo'},
+            { data: 'FechaCobro', title: 'FechaCobro'},
             { data: 'Glosa', title: 'Glosa'},
             { data: 'Usuario', title: 'Usuario'},
             { data: 'Moneda', title: 'M.'},
@@ -116,8 +116,8 @@ $(document).ready(function()
         ],
         "pageLength": 100,  
         "columnDefs": [
-            { className: "dt-right", "targets":[4,5,6]},
-            { className: "sum_total", "targets":[4]},
+            { className: "dt-right", "targets":[6,7,8]},
+            { className: "sum_total", "targets":[6]},
             { className: "categoria_max", "targets":[1,7]}
         ],
         "language":             
@@ -149,9 +149,9 @@ $(document).ready(function()
         //var sum = table.column(4).data();
         //var tab = table.rows( { selected: true, search: 'applied' } ).data();
         //console.log(tab); 
-        var sumTCXC = table.column( 4, {search:'applied'} ).data().sum();
-        var sumTCuenta = table.column( 5, {search:'applied'} ).data().sum();
-        var sumTSaldo = table.column( 6, {search:'applied'} ).data().sum();
+        var sumTCXC = table.column( 6, {search:'applied'} ).data().sum();
+        var sumTCuenta = table.column( 7, {search:'applied'} ).data().sum();
+        var sumTSaldo = table.column( 8, {search:'applied'} ).data().sum();
         sumTCXC = Math.round((sumTCXC + Number.EPSILON) * 100) / 100;
         sumTCuenta = Math.round((sumTCuenta + Number.EPSILON) * 100) / 100;
         sumTSaldo = Math.round((sumTSaldo + Number.EPSILON) * 100) / 100;
@@ -176,23 +176,23 @@ $(document).ready(function()
         .filter( function ( value, index ) {
             return 'MORA' === table.row(value).data()['estado'];
         } );
-        sumVigenteCXC = Math.round((table.cells(vigente, 4).data().sum() + Number.EPSILON) * 100) / 100;
-        sumVigenteCuenta = Math.round((table.cells(vigente, 5).data().sum() + Number.EPSILON) * 100) / 100;
-        sumVigenteCSaldo = Math.round((table.cells(vigente, 6).data().sum() + Number.EPSILON) * 100) / 100;
+        sumVigenteCXC = Math.round((table.cells(vigente, 6).data().sum() + Number.EPSILON) * 100) / 100;
+        sumVigenteCuenta = Math.round((table.cells(vigente, 7).data().sum() + Number.EPSILON) * 100) / 100;
+        sumVigenteCSaldo = Math.round((table.cells(vigente, 8).data().sum() + Number.EPSILON) * 100) / 100;
         $('.dataTables_scrollFootInner .sumVigenteCXC').html(sumVigenteCXC.toFixed(2));
         $('.dataTables_scrollFootInner .sumVigenteCuenta').html(sumVigenteCuenta.toFixed(2));
         $('.dataTables_scrollFootInner .sumVigenteSaldo').html(sumVigenteCSaldo.toFixed(2));
 
-        sumVencidoCXC = Math.round((table.cells(vencido, 4).data().sum() + Number.EPSILON) * 100) / 100;
-        sumVencidoCuenta = Math.round((table.cells(vencido, 5).data().sum() + Number.EPSILON) * 100) / 100;
-        sumVencidoCSaldo = Math.round((table.cells(vencido, 6).data().sum() + Number.EPSILON) * 100) / 100;
+        sumVencidoCXC = Math.round((table.cells(vencido, 6).data().sum() + Number.EPSILON) * 100) / 100;
+        sumVencidoCuenta = Math.round((table.cells(vencido, 7).data().sum() + Number.EPSILON) * 100) / 100;
+        sumVencidoCSaldo = Math.round((table.cells(vencido, 8).data().sum() + Number.EPSILON) * 100) / 100;
         $('.dataTables_scrollFootInner .sumVencidoCXC').html(sumVencidoCXC.toFixed(2));
         $('.dataTables_scrollFootInner .sumVencidoCuenta').html(sumVencidoCuenta.toFixed(2));
         $('.dataTables_scrollFootInner .sumVencidoSaldo').html(sumVencidoCSaldo.toFixed(2));
 
-        sumMoraCXC = Math.round((table.cells(mora, 4).data().sum() + Number.EPSILON) * 100) / 100;
-        sumMoraCuenta = Math.round((table.cells(mora, 5).data().sum() + Number.EPSILON) * 100) / 100;
-        sumMoraCSaldo = Math.round((table.cells(mora, 6).data().sum() + Number.EPSILON) * 100) / 100;
+        sumMoraCXC = Math.round((table.cells(mora, 6).data().sum() + Number.EPSILON) * 100) / 100;
+        sumMoraCuenta = Math.round((table.cells(mora, 7).data().sum() + Number.EPSILON) * 100) / 100;
+        sumMoraCSaldo = Math.round((table.cells(mora, 8).data().sum() + Number.EPSILON) * 100) / 100;
         $('.dataTables_scrollFootInner .sumMoraCXC').html(sumMoraCXC.toFixed(2));
         $('.dataTables_scrollFootInner .sumMoraCuenta').html(sumMoraCuenta.toFixed(2));
         $('.dataTables_scrollFootInner .sumMoraSaldo').html(sumMoraCSaldo.toFixed(2));
